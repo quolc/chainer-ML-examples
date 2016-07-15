05876f89083b27b700d3795bf10f91b0e451afc6
Ryohei Suzuki <quolc.i@gmail.com>
2016-07-15 21:37:30 +0900

    remove duplicated code

--- train_linear.py
+++ train_linear.py
@@ -31,28 +31,6 @@ for i in range(0, N):
 train_data = np.array(train_data_raw, dtype="float32")
 train_target = np.array(train_target_raw, dtype="int32")
 
-# (2,2) 単純パーセプトロン
-class SimplePerceptron(Chain):
-    def __init__(self):
-        super(SimplePerceptron, self).__init__(
-            l1=L.Linear(2, 2),
-        )
-
-    def __call__(self, x):
-        return F.relu(self.l1(x))
-
-# 分類器
-class Classifier(Chain):
-    def __init__(self, predictor):
-        super(Classifier, self).__init__(predictor=predictor)
-
-    def __call__(self, x, t):
-        y = self.predictor(x)
-        self.loss = F.softmax_cross_entropy(y, t)
-        self.accuracy = F.accuracy(y, t)
-        report({'loss': self.loss, 'accuracy': self.accuracy}, self)
-        return self.loss
-
 # モデルインスタンスの作成
 model = Classifier(SimplePerceptron())
 
