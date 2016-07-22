23b210c83d4a76d0b2bba42766b4140faa985473
Ryohei Suzuki <quolc.i@gmail.com>
2016-07-22 19:38:21 +0900

    implement basic learning routine

---
+++ net.py
@@ -0,0 +1,27 @@
+import chainer
+import chainer.functions as F
+import chainer.links as L
+from chainer import report
+
+class AutoEncoder(chainer.Chain):
+    def __init__(self, n_in, n_units, activation):
+        super(AutoEncoder, self).__init__(
+            l1 = L.Linear(n_in, n_units),
+            l2 = L.Linear(n_units, n_in)
+        )
+        self.activation = {'relu': F.relu, 'sigmoid': F.sigmoid}[activation]
+
+    def __call__(self, x, train):
+        h1 = F.dropout(self.activation(self.l1(x)), train=train)
+        return F.dropout(self.l2(h1), train=train)
+
+class Regression(chainer.Chain):
+    def __init__(self, predictor):
+        super(Regression, self).__init__(predictor=predictor)
+
+    def __call__(self, x, t):
+        y = self.predictor(x, True)
+        self.loss = F.mean_squared_error(y, t)
+        report({'loss': self.loss}, self)
+        return self.loss
+
