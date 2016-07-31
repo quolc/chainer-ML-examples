d1a6d0a53d15dffe23a77b8087241ceca77d2194
Ryohei Suzuki <quolc.i@gmail.com>
2016-07-31 21:28:28 +0900

    support tied weight on dump_mnist_sae.py

--- mnist-stacked-autoencoder/dump_mnist_sae.py
+++ mnist-stacked-autoencoder/dump_mnist_sae.py
@@ -43,6 +43,7 @@ parser.add_argument('--unit', '-u', default='1000,500,250,2',
                     help='number of units')
 parser.add_argument('--activation', '-a', choices=('relu', 'sigmoid'),
                     default='relu', help="activation function")
+parser.add_argument('--untied', '-t', action='store_const', const=True, default=False)
 
 args = parser.parse_args()
 
@@ -55,6 +56,7 @@ print()
 print('[settings]')
 print('- activation func: %s' % activation)
 print('- structure: ', n_units)
+print('- tied: ', not(args.untied))
 
 # prepare dataset
 print()
@@ -74,7 +76,7 @@ aes = []
 for idx in range(len(n_units)):
     n_in = n_units[idx-1] if idx > 0 else 28*28
     n_out = n_units[idx]
-    aes.append(AutoEncoder(n_in, n_out, activation))
+    aes.append(AutoEncoder(n_in, n_out, activation, not(args.untied)))
 
 model = Regression(StackedAutoEncoder(aes))
 serializers.load_npz(args.model, model)
@@ -88,16 +90,17 @@ print()
 print('# dumping images')
 for l in range(1, len(model.predictor)+1):
     print('layer %d' % l)
-    y = model.predictor(x, depth=l)
+    y = model.predictor(x, train=False, depth=l)
     for i in range(math.ceil(args.num/10)):
-        print('.', end="")
         sys.stdout.flush()
         for j in range(10):
             n = i*10 + j
+            if n >= args.num: break
             n1 = (i*2)*10 + j
             n2 = (i*2+1)*10 + j
             draw_digit_ae(x_train[perm[n]], n1, math.ceil(args.num/10)*2, 10)
             draw_digit_ae(y.data[n], n2, math.ceil(args.num/10)*2, 10)
+        print('.', end="")
     print()
     plt.savefig('dump_{}.png'.format(l))
 
