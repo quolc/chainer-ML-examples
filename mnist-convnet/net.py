7f22351f3cc149a409414ebc48cde5c4d0c43433
Ryohei Suzuki <quolc.i@gmail.com>
2016-08-05 19:17:28 +0900

    add elementary implementation of MNIST CNN

---
+++ mnist-convnet/net.py
@@ -0,0 +1,49 @@
+import chainer
+import chainer.functions as F
+import chainer.links as L
+
+# 1-conv 1-hidden convolutional network
+#   input: 28px * 28px * 1ch
+#   output: 10 units (label: 0-9)
+#
+#   structure: CONV -> RELU -> POOL -> FC1 -> FC2
+#     - CONV: 28px * 28px * 1ch -> 26px * 26px * 8ch
+#             3*3 filter, 0 padding
+#     - FC1:  1352units -> 256units
+#     - FC2:  256units -> 10units
+# 
+class ConvNet(chainer.Chain):
+    def __init__(self, dim_x, dim_y, n_ch_in, n_ch_conv, n_unit1, n_out,
+                 ksize=3, activation='relu'):
+        padding_loss = ksize-1
+        dim_x_conv = dim_x - padding_loss
+        dim_y_conv = dim_y - padding_loss
+
+        super(ConvNet, self).__init__(
+            conv1 = L.Convolution2D(
+                in_channels=n_ch_in, out_channels=n_ch_conv,
+                ksize=ksize, pad=0),
+            l1 = L.Linear(
+                (dim_x_conv//2) * (dim_y_conv//2) * n_ch_conv,
+                n_unit1),
+            l2 = L.Linear(n_unit1, n_out)
+        )
+        self.activation = {'relu': F.relu, 'sigmoid': F.sigmoid, 'identity': F.identity}[activation]
+
+    def __call__(self, x, train=True):
+        h1 = F.max_pooling_2d(self.activation(self.conv1(x)), 2)
+        h2 = F.dropout(self.activation(self.l1(h1)), train=train)
+        return self.activation(self.l2(h2))
+
+class Classifier(chainer.Chain):
+    def __init__(self, predictor):
+        super(Classifier, self).__init__(
+            predictor = predictor
+        )
+
+    def __call__(self, x, t, train=False):
+        y = self.predictor(x, train)
+        self.loss = F.softmax_cross_entropy(y, t)
+        self.accuracy = F.accuracy(y, t)
+        return self.loss
+
---
+++ mnist-convnet/train_mnist_cnn.py
@@ -0,0 +1,120 @@
+import argparse
+import sys, time, datetime
+
+import numpy as np
+
+import chainer
+from chainer import optimizers, serializers, cuda
+
+import data
+import net
+
+parser = argparse.ArgumentParser(description="MNIST Convolutional Neural Network Training")
+# file loading
+
+# computation / learning settings
+parser.add_argument('--gpu', '-g', type=int, default=-1,
+                    help='GPU ID (negative value indicates CPU)')
+parser.add_argument('--epoch', '-e', default=20, type=int,
+                    help='number of epochs for training')
+parser.add_argument('--batchsize', '-b', type=int, default=100,
+                    help='learning minibatch size')
+parser.add_argument('--optimizer', '-o', choices=('adam', 'momentumsgd'),
+                    default='adam', help='optimizer (adam or momentumsgd)')
+parser.add_argument('--learningrate', type=float, default=0.01,
+                    help='learning rate (momentum SGD only)')
+parser.add_argument('--alpha', type=float, default=0.001,
+                    help='alpha value (Adam only)')
+
+# structure settings
+# ToDo: prepare setting file
+
+args = parser.parse_args()
+
+batchsize = args.batchsize
+n_epoch = args.epoch
+
+print()
+print('[settings]')
+print('- GPU: %d' % args.gpu)
+print('- minibatch-size: %d' % batchsize)
+print('- epoch: %d' % n_epoch)
+
+print('- optimizer: %s' % args.optimizer)
+if args.optimizer == 'momentumsgd':
+    print('- learning rate: %f' % args.learningrate)
+if args.optimizer == 'adam':
+    print('- alpha: %f' % args.alpha)
+
+# GPU setup
+xp = np
+if args.gpu >= 0:
+    chainer.cuda.get_device(args.gpu).use()
+    xp = cuda.cupy
+
+# prepare dataset
+print()
+print('# loading MNIST dataset')
+mnist = data.load_mnist_data()
+mnist['data'] = mnist['data'].astype(xp.float32)
+mnist['data'] /= 255
+mnist['target'] = mnist['target'].astype(xp.int32)
+
+N = data.num_train
+N_test = data.num_test
+x_train, x_test = xp.split(mnist['data'], [N])
+y_train, y_test = xp.split(mnist['target'], [N])
+
+# convert to tensor representation
+x_train = x_train.reshape((len(x_train), 1, 28, 28))
+x_test = x_test.reshape((len(x_test), 1, 28, 28))
+
+print('- number of training data: %d' % N)
+print('- number of test data: %d' % N_test)
+print('done.')
+
+# prepare network
+model = net.Classifier(net.ConvNet(28, 28, 1, 8, 256, 10))
+
+# initialize optimizer
+if args.optimizer == 'adam':
+    optimizer = optimizers.Adam(args.alpha)
+if args.optimizer == 'momentumsgd':
+    optimizer = optimizers.MomentumSGD(args.learningrate)
+optimizer.setup(model)
+
+# training loop
+print()
+print('start learning')
+if args.gpu >= 0:
+    model.to_gpu()
+for epoch in range(0, n_epoch):
+    print('epoch', epoch+1)
+
+    perm = xp.random.permutation(N)
+    permed_data = xp.array(x_train[perm])
+    permed_target = xp.array(y_train[perm])
+
+    sum_accuracy = 0
+    sum_loss = 0
+
+    start = time.time()
+    for i in range(0, N, batchsize):
+        if i % 10000 == 0:
+            print(i)
+        x = chainer.Variable(permed_data[i:i+batchsize])
+        y = chainer.Variable(permed_target[i:i+batchsize])
+
+        optimizer.update(model, x, y)
+        sum_loss += float(model.loss.data) * len(y.data)
+        sum_accuracy += float(model.accuracy.data) * len(y.data)
+    end = time.time()
+    elapsed_time = end - start
+    throughput = N / elapsed_time
+
+    print('train mean loss={}, accuracy={}, throughput={} images/sec'.format(
+        sum_loss / N, sum_accuracy / N, throughput))
+
+print('save the model') 
+serializers.save_npz('linear.model', model)
+
